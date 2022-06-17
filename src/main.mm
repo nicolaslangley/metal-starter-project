8d95eea400c70580ebdcaf4340df9522971ab6b0
Nicolas Langley <nicolaslangley@hey.com>
2022-06-17 12:03:35 -0400

    Add initial no-nib Metal application setup
    Also adds build.zig and XCode project for building and debugging

---
+++ src/main.mm
@@ -0,0 +1,122 @@
+#import <Cocoa/Cocoa.h>
+#import <Metal/Metal.h>
+#import <MetalKit/MetalKit.h>
+
+@interface MTKViewDelegate: NSObject<MTKViewDelegate>
+-(nonnull instancetype)initWithMetalKitView:(nonnull MTKView *)view;
+@end
+
+@implementation MTKViewDelegate
+{
+  id<MTLDevice> _device;
+  id<MTLCommandQueue> _queue;
+}
+
+-(nonnull instancetype)initWithMetalKitView:(nonnull MTKView *)view;
+{
+  self = [super init];
+  if (self)
+  {
+    _device = view.device;
+    _queue = [_device newCommandQueue];
+  }
+  return self;
+}
+
+- (void)mtkView:(nonnull MTKView *)view drawableSizeWillChange:(CGSize)size
+{
+  
+}
+
+- (void)drawInMTKView:(nonnull MTKView *)view
+{
+  id<MTLCommandBuffer> commandBuffer = [_queue commandBuffer];
+  MTLRenderPassDescriptor* renderPassDescriptor = view.currentRenderPassDescriptor;
+  renderPassDescriptor.colorAttachments[0].clearColor = MTLClearColorMake(1.0, 0.0, 0.0, 1.0);
+  id<MTLRenderCommandEncoder> renderEncoder = [commandBuffer renderCommandEncoderWithDescriptor:renderPassDescriptor];
+  [renderEncoder endEncoding];
+  [commandBuffer presentDrawable:view.currentDrawable];
+  [commandBuffer commit];
+}
+@end
+
+@interface NSWindowDelegate : NSObject<NSWindowDelegate>
+@end
+
+@implementation NSWindowDelegate
+- (BOOL)windowShouldClose:(id)sender {
+  return YES;
+}
+
+- (void)windowDidResize:(NSNotification*)notification {
+}
+@end
+
+@interface AppDelegate : NSObject<NSApplicationDelegate>
+@end
+
+@implementation AppDelegate
+{
+  NSWindow* _window;
+  MTKView* _view;
+  MTKViewDelegate* _viewDelegate;
+  NSWindowDelegate* _windowDelegate;
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification*)aNotification
+{
+  NSRect screen_rect = NSScreen.mainScreen.frame;
+  const NSUInteger style =
+          NSWindowStyleMaskTitled |
+          NSWindowStyleMaskClosable |
+          NSWindowStyleMaskMiniaturizable |
+          NSWindowStyleMaskResizable;
+  const float window_scale = 4.0 / 5.0;
+  NSRect window_rect = NSMakeRect(0, 0, screen_rect.size.width * window_scale, screen_rect.size.height * window_scale);
+  _window = [[NSWindow alloc]
+          initWithContentRect:window_rect
+          styleMask:style
+          backing:NSBackingStoreBuffered
+          defer:NO];
+  _windowDelegate = [[NSWindowDelegate alloc] init];
+  [_window setDelegate:_windowDelegate];
+  
+  _view = [[MTKView alloc] initWithFrame:window_rect];
+  id<MTLDevice> device = MTLCreateSystemDefaultDevice();
+  [_view setDevice:device];
+  [_view setColorPixelFormat:MTLPixelFormatBGRA8Unorm];
+  [_view setDepthStencilPixelFormat:MTLPixelFormatDepth32Float_Stencil8];
+  _viewDelegate = [[MTKViewDelegate alloc] initWithMetalKitView:_view];
+  [_view setDelegate:_viewDelegate];
+  
+  [_window setContentView:_view];
+  [_window makeFirstResponder:_view];
+  
+  [_window center];
+  [NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
+  [NSApp activateIgnoringOtherApps:YES];
+  [_window makeKeyAndOrderFront:nil];
+}
+
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication*)sender 
+{
+    return YES;
+}
+
+- (void)applicationWillTerminate:(NSNotification*)notification 
+{
+}
+@end
+
+int main(int argc, const char * argv[]) 
+{
+  @autoreleasepool {
+    [NSApplication sharedApplication];
+    
+    AppDelegate* appDelegate = [[AppDelegate alloc] init];
+    [NSApp setDelegate:appDelegate];
+    
+    [NSApp run];
+  }
+  return 0;
+}
